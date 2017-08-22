0973e7ee60d62a99eeec11707a7b69294b96ee51
muteti254 <thomas.muteti@gmail.com>
2017-08-22 13:17:59 +0300

    sites edits

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from geonode.contrib.geosites.urls import urlpatterns, handler403
 
 from geonode.urls import *
 
