b8216d21887ebfc96eb4f13f5dba3de01a29056f
muteti254 <thomas.muteti@gmail.com>
2017-08-22 13:00:49 +0300

    setup file edits

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="Thomas Muteti",
     author_email="",
     description="{{ project_name }}, based on GeoNode",
-    long_description=(read('README.rst')),
+    long_description=(read('README.md')),
     # Full list of classifiers can be found at:
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ find . -type f ! -name setup.sh -exec sed -i "s|{{ root_path }}|$PWD/..|g" {} +
 find . -type f ! -name setup.sh -exec sed -i "s/{{ domain }}/$DOMAIN/g" {} +
 
 # install virtualenv and geonode
-pip install -e .
+sudo pip install -e .
 mkdir logs
